6a7d7152ff20dd6427fbd30789b73deea131497f
RasyaJusticio <rasyajusticio.personal@gmail.com>
2025-06-02 08:37:08 +0700

    feat(iframe-guard): make iframe guard works based on the current environtment

--- routes/web.php
+++ routes/web.php
@@ -7,22 +7,22 @@ use App\Http\Controllers\Qurani\JuzController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-//backend
-
-
 Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+Route::get('/filter', function () {
+    return Inertia::render('dashboard/recap');
+})->name('filter');
+
+Route::get('/recap', function () {
+    return Inertia::render('recap/index');
+})->name('recap');
+
 Route::get('/surah/{surah}', [ChapterController::class, 'show'])
     ->name('surah');
 Route::get('/juz/{juz}', [JuzController::class, 'show'])->name('juz');
 
-
 Route::get('/redirect', function () {
     return Inertia::render('redirect');
 })->name('redirect');
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-Route::get('/filter', function () {
-    return Inertia::render('dashboard/recap');
-})->name('filter');
-Route::get('/recap', function () {
-    return Inertia::render('recap/index');
-})->name('recap');
